c32679cfdf3d620ae2f93a05229f6f0ac54f50f9
dai-motoki <dai.motoki1123@gmail.com>
2024-07-03 02:27:43 +0900

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zoltraak",
-    version="0.1.32",
+    version="0.1.33",
     packages=find_packages(),
     # package_dir={'': '.'},  # ここでベースディレクトリを指定
     install_requires=[
@@ -14,6 +14,7 @@ setup(
         "wheel",
         "diagrams"
         "google-api-python-client",  # Google APIクライアントライブラリを追加
+        "google-generativeai",  # Google の生成 AI ライブラリを追加
     ],
     package_data={
         '': ['*.txt', '*.md', '*.json', '*.csv', '*.yaml', '*.yml'],
